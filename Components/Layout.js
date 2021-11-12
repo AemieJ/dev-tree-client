f12cc86d664aa1e63bba4852bb10e23233462566
AemieJ <aemie.j@hotmail.co.uk>
2021-11-12 18:00:37 +0530

    implement scroll up

--- Components/Layout.js
+++ Components/Layout.js
@@ -1,5 +1,6 @@
 import Header from './Header.js'
 import Footer from './Footer.js'
+import ScrollTop from './ScrollTop.js'
 import { Container } from 'react-bootstrap'
 import styles from '../styles/Home.module.css'
 
@@ -7,6 +8,7 @@ const Layout = ({ children }) => {
   return (
     <>
     <Header />
+    <ScrollTop />
     <Container className={styles.container}>
       {children}
       <Footer />
